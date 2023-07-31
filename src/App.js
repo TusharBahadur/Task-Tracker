f2ed9d1e83d9a8f0c8e14bef19f80d378595ac1e
TusharBahadur~ <tusharbahadur001@gmail.com>
2023-08-01 00:57:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+
+
+
+import { useState } from "react"
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id
+    ))
+  }
+
+  const reminders = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+  return (
+
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={reminders} /> : ("No Tasks")}
+      <Footer />
+    </div>
+
+
+  )
+}
+
+export default App;
